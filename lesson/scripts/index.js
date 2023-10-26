524205bf808ce57768714d08848939bc8845d4cf
evgenii.tarasenko <evgenii.tarasenko@uadevelopers.com>
2023-10-26 20:24:54 +0200

    Lesson 10

---
+++ lesson/scripts/index.js
@@ -0,0 +1,26 @@
+let amountForUser;
+const PRESENT_AMOUNT = 11;
+
+// ..
+/*
+ * ...
+ */
+
+amountForUser = prompt('Fill ...');
+amountForUser = +amountForUser + PRESENT_AMOUNT;
+alert('Your amount will bw - ' + amountForUser);
+
+let x;
+x = 5;
+console.log(--x);
+console.log(++x);
+
+let y;
+y = 5;
+y -= 1;
+console.log(y);
+// y = y + 1;
+y += 1;
+console.log(y);
+
+let result = x === y;
