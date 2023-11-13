1da3a0bfaaeb9d8f77a3327e19243d4055d89fe6
evgenii.tarasenko <evgenii.tarasenko@uadevelopers.com>
2023-11-13 17:03:51 +0100

    Lesson 15 - wip

--- todo/scripts/list.js
+++ todo/scripts/list.js
@@ -1,5 +1,5 @@
 /*
- * Modal processing
+ * Modal handling
  */
 function showFormModal() {
     document.getElementById('form-modal').classList.add('d-block');
@@ -12,48 +12,62 @@ function hideFormModal() {
 /*
  * Actions
  */
-function create () {
-    // Clear existing input value
-    document.getElementById('form-title').value = '';
+function init() {
+    const list = getToDoList();
+
+    list.forEach((item) => createToDoHtmlItem(item));
+}
+
+function create() {
     document.getElementById('form-uuid').value = '';
-    clearValidationErrors();
+    document.getElementById('form-title').value = '';
+
     showFormModal();
 }
 
 function edit(uuid) {
-    const title = document.getElementById(`item-${uuid}`).querySelector('.title').innerText;
+    const title = document.getElementById(`title-${uuid}`).innerText;
+
     document.getElementById('form-title').value = title;
     document.getElementById('form-uuid').value = uuid;
-
-    clearValidationErrors();
     showFormModal();
 }
 
 function remove(uuid) {
-    // TODO: confirmation
-
-    document.getElementById(`item-${uuid}`).remove();
+    const decision = confirm('Are you sure?');
+    if (decision) {
+        document.getElementById(`item-${uuid}`).remove();
+    }
 }
 
 function save(data) {
-    const uuid = data.uuid ? data.uuid : generateUuid();
-
-    // Updating
-    const editedLi = document.getElementById(`item-${uuid}`);
-    if (editedLi) {
-        editedLi.querySelector('.title').innerText = data.title;
-
-        return;
+    if (!data.uuid) {
+        data.uuid = generateUuid();
     }
 
-    // Storing
+    let list = getToDoList();
+
+    const editableItemIndex = list.findIndex((item) => item.uuid === data.uuid);
+
+    if (editableItemIndex !== -1) {
+        list[editableItemIndex] = data;
+        updateToDoHtmlItem(data);
+    } else {
+        list.push(data);
+        createToDoHtmlItem(data);
+    }
+
+    setToDoList(list);
+}
+
+function createToDoHtmlItem(data) {
     let liElement = document.createElement('li');
-    liElement.id = `item-${uuid}`;
+    liElement.id = `item-${data.uuid}`;
     liElement.innerHTML = `
-        <div class="title">${data.title}</div>
+        <div id="title-${data.uuid}">${data.title}</div>
         <div>
-            <button data-uuid="${uuid}" type="button" class="btn btn-warning btn-sm edit-button">Edit</button>
-            <button data-uuid="${uuid}" type="button" class="btn btn-danger btn-sm remove-button">Remove</button>
+            <button data-uuid="${data.uuid}" class="btn btn-warning btn-sm edit-button">Edit</button>
+            <button data-uuid="${data.uuid}" class="btn btn-danger btn-sm remove-button">Remove</button>
         </div>`;
     liElement.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center');
 
@@ -61,40 +75,55 @@ function save(data) {
         edit(event.target.dataset.uuid);
     });
 
-    liElement.querySelector('.remove-button').addEventListener('click', function (event) {
-       remove(event.target.dataset.uuid);
+    liElement.querySelector('.remove-button').addEventListener('click', function (event ) {
+        remove(event.target.dataset.uuid);
     });
 
     document.getElementById('todo-list').appendChild(liElement);
 }
 
+function updateToDoHtmlItem(data) {
+    const editedLi = document.getElementById(`item-${data.uuid}`);
+
+    if (editedLi) {
+        editedLi.querySelector(`#title-${data.uuid}`).innerText = data.title;
+    }
+}
+
 /*
  * Listeners
  */
-document.getElementById("saving-form").addEventListener("submit", function (event) {
+document.getElementById('form').addEventListener('submit', function (event) {
     event.preventDefault();
 
-    const uuid = document.getElementById('form-uuid').value;
     const title = document.getElementById('form-title').value;
+    const uuid = document.getElementById('form-uuid').value;
 
     const data = {
         uuid,
         title,
-    }
+    };
 
-    if (validate(data)) {
+    if (validateForm(data)) {
         save(data);
         hideFormModal();
     }
 });
 
-function validate(data) {
-    clearValidationErrors();
+window.addEventListener('load', function () {
+    init();
+})
+
+/*
+ * Validation
+ */
+function validateForm(data) {
+    clearErrors();
 
     let decision = true;
 
     if (!data.title) {
-        document.getElementById('form-title-invalid-feedback').innerText = 'Title field is required.'
+        document.getElementById('form-title-invalid-feedback').innerText = 'Title field is required.';
         document.getElementById('form-title').classList.add('is-invalid');
 
         decision = false;
@@ -103,8 +132,21 @@ function validate(data) {
     return decision;
 }
 
-function clearValidationErrors () {
+function clearErrors() {
     document.getElementById('form-title').classList.remove('is-invalid');
+}
+
+/*
+ * Local storage
+ */
+function getToDoList() {
+    const list = localStorage.getItem('todo-list');
+
+    return list ? JSON.parse(list) : [];
+}
+
+function setToDoList(list) {
+    localStorage.setItem('todo-list', JSON.stringify(list));
 }
 
 /*
